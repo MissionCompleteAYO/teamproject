9badab03fea365d476675c42531b2126265e5134
lsj <akaakaldk1234@naver.com>
2023-10-19 20:26:52 +0900

    4수정

--- src/main/java/com/example/teamproject/TeamprojectApplication.java
+++ src/main/java/com/example/teamproject/TeamprojectApplication.java
@@ -2,8 +2,9 @@ package com.example.teamproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TeamprojectApplication {
 
 	public static void main(String[] args) {
