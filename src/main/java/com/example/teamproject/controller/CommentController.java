70ebb5bedd4def88d79d963b90390e3d18718232
jam2208 <junwoo.park82@icloud.com>
2023-10-20 16:00:12 +0900

    comment feature modifying

--- src/main/java/com/example/teamproject/controller/CommentController.java
+++ src/main/java/com/example/teamproject/controller/CommentController.java
@@ -1,21 +1,44 @@
 package com.example.teamproject.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.teamproject.repository.CommentRepository;
 
 @Controller
-@RequestMapping("/company")
+@RequestMapping("/store")
 public class CommentController {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    HttpSession session;
+
+    @GetMapping("/comment")
+    @ResponseBody
+    public String comment(@ModelAttribute User user) {
+        Optional<User> dbUser = session.getAttribute("user_info");
+        commentRepository.findByUserId()
+        
+    }
     
     @GetMapping("/detail")
     public String detail() {
-        return "/company/detail";
+        return "/store/detail";
     }
 
     @PostMapping("/detail")
     public String detailPost() {
-        return "redirect:company/detail";
+
+
+        return "redirect:store/detail";
     }
 }
--- src/main/java/com/example/teamproject/model/Comment.java
+++ src/main/java/com/example/teamproject/model/Comment.java
@@ -14,16 +14,12 @@ public class Comment {
     @Id
     @GeneratedValue
     Long id;
-
-    String name;
     
     String comment;
     
-    Integer flavor;
+    Integer like;
     
-    Integer kind;
-    
-    Integer clean;
+    Integer unlike;
 
     Long reviewId;
 
