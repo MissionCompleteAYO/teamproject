1ff4a25dbddd3c72588304d1c74943aece25345e
jam2208 <junwoo.park82@icloud.com>
2023-10-20 09:28:11 +0900

    url modify

--- src/main/java/com/example/teamproject/controller/CommentController.java
+++ src/main/java/com/example/teamproject/controller/CommentController.java
@@ -11,7 +11,7 @@ public class CommentController {
     
     @GetMapping("/detail")
     public String detail() {
-        return "company/detail";
+        return "/company/detail";
     }
 
     @PostMapping("/detail")
